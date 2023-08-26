1606e979f949862da7da8651246357baba744be6
Zola <zolboo@alumni.stanford.edu>
2023-08-26 15:31:04 -0700

    change mongoose connection

--- server.js
+++ server.js
@@ -14,35 +14,33 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 import indexRouter from './routes/index.js';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, "views"));
-app.set('layout', "layouts/layout");
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
 import mongoose from 'mongoose';
 
-main().catch(err => console.log(err));
-
-async function main() {
-  console.log('here');
-  await mongoose.connect(process.env.DATABASE_URL).then(() => {
-    app.listen(process.env.PORT || port, () => {
-      console.log(`listening on ${port}`);
-    });
-  });
-  console.log('hi');
-  const db = mongoose.connection;
-
-  db.on('error', (error) => {
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
     console.log(error);
-  });
-  db.once('open', () => {
-    console.log('connection established');
-  });
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
+    process.exit(1);
+  }
+};
 
 app.use('/', indexRouter);
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('listening for requests');
+  });
+});
+
+
