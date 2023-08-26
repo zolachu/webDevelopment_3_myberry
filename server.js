5532ba212e6901c3eb1f1e91b2a701e8789f12c5
Zola <zolboo@alumni.stanford.edu>
2023-08-26 15:52:58 -0700

    change port t0 3000

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 import indexRouter from './routes/index.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
